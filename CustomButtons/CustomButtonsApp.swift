c5a510344568ec56d345d69148ad24d57dcadce2
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2020-11-09 07:07:34 +0000

    Initial Commit

---
+++ CustomButtons/CustomButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButtonsApp.swift
+//  CustomButtons
+//
+//  Created by Tony Hajdini on 09/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct CustomButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
