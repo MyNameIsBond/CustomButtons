db6d0ae95f57bca3bdeeab48901a98ad271972b1
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2020-11-10 04:43:29 +0000

    cc

--- CustomButtons/ContentView.swift
+++ CustomButtons/ContentView.swift
@@ -1,16 +1,44 @@
-//
-//  ContentView.swift
-//  CustomButtons
-//
-//  Created by Tony Hajdini on 09/11/2020.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        RadiantButton(startColor: Color.yellow, endColor: Color.red)
+    }
+}
+
+
+struct RadiantButton: View {
+    
+    var startColor: Color
+    var endColor: Color
+    
+    var body: some View {
+        Button(action: {}) {
+            HStack {
+                Image(systemName: "heart").foregroundColor(Color.primary)
+                Text("Like The Post").foregroundColor(Color.primary)
+            }
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .background(LinearGradient(gradient: Gradient(colors: [startColor, endColor]), startPoint: .topLeading, endPoint: .bottomTrailing))
+        .cornerRadius(20)
+        .shadow(radius: 10)
+    }
+}
+
+struct ButtonStyle: View {
+    var body: some View {
+        VStack {
+            Button("Plain", action: {
+            }).buttonStyle(PlainButtonStyle())
+
+            Button("Borderless", action: {
+            }).buttonStyle(BorderlessButtonStyle())
+
+            Button("Default", action: {
+            }).buttonStyle(DefaultButtonStyle())
+        }
     }
 }
 
